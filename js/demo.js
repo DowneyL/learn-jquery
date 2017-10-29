8420bd731c5bcba47c1d28c417d2e18ced2fcc84
AragakiYuiCyann <heng8050lee@foxmail.com>
2017-10-29 23:25:47 +0800

    lesson 4 half

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,41 @@
 $(function (){
-    
+    // 基本过滤选择器
+
+    // 基本过滤选择器一 :first
+    // $('li:first').css('background', '#ccc');
+
+    // 基本过滤选择器二 :last
+    // $('li:last').css('background', '#66ccff');
+
+    // 基本过滤选择器三 :gt
+    // $('li:gt(9)').css('background', '#00f');
+
+    // 基本过滤选择器四 :lt
+    // $('li:lt(2)').css('background', '#f0f');
+
+    // 基本过滤选择器五 :even (从 0 开始算)
+    // $('li:even').css('background', '#ccc');
+
+    // 基本过滤选择器六 :odd
+    // $('li:odd').css('background', '#66ccff');
+
+    // 基本过滤选择器七 :eq
+    // $('li:eq(2)').css('background', '#f00');
+
+    // 基本过滤选择器八 :not
+    // $('li:not(:eq(1))').css('background', '#f00');
+
+    // 基本过滤选择器九 :header
+    $(':header').css('background', '#ccc');
+
+    // 对应 jquery 方法
+    // $('li').first().css('background','#ccc');
+
+    // $('li').last().css('background','#ccc');
+
+    // $('li').eq(2).css('background','#ccc');
+
+    // $('li').not(':eq(1)').css('background', '#ccc');
+
+
 });
